cda63e5c5cbe027551b86bcd991d52faf707a671
Unknown <risahawkeye@mail.ru>
2018-07-31 15:41:33 +0300

    change init

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
