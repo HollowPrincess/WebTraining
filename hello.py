f00365d0234e4a28b806f4f8d1f683aee4c39d31
Unknown <risahawkeye@mail.ru>
2018-08-21 15:30:52 +0300

    wsgi

---
+++ hello.py
@@ -0,0 +1,10 @@
+from cgi import parse_qs
+
+def app(env,start_response):
+ status='200 OK'
+ headers=[
+  ('Content-Type','text/plain')
+ ]
+ body=[env['QUERY_STRING'].replace('&', '\n\'')]
+ start_response(status,headers)
+return body
