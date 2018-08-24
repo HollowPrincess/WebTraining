2d508bb20cdcb830c00c9b150081e412c24990fb
Unknown <risahawkeye@mail.ru>
2018-08-24 19:46:28 +0300

    fix name

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo rm /etc/gunicorn.d/ask
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello.wsgi:application
-sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf wsgi:application
 
 #sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 #sudo /etc/init.d/gunicorn restart
