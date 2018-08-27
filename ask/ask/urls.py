2d8869c0cd4e8c796856bb01edeea5aa0d884377
Unknown <risahawkeye@mail.ru>
2018-08-27 18:38:03 +0300

    few more changes

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,12 +17,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('qa.urls')), #URL / 
-    url(r'^login/$', include('qa.urls')), #URL /login/
-    url(r'^signup/$', include('qa.urls')), #URL /signup/
-    url(r'^question/<(?P<ID>[0-9])+>/$', include('qa.urls')), #URL /question/<ID>
-    url(r'^ask/$', include('qa.urls')), #URL /ask/
-    url(r'^popular/$', include('qa.urls')), #URL /popular/
-    url(r'^new/$', include('qa.urls')), #URL /new/
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'*', include(r'^$', 'ask.views.home', name='home'),
 ]
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'*', include(ask.qa.views.test), name='test'),
+    url(r'^$', include('qa.urls')), #URL / 
+    url(r'^login/', include('qa.urls')), #URL /login/
+    url(r'^signup/', include('qa.urls')), #URL /signup/
+    url(r'^question/(?P<ID>[0-9])+/', include('qa.urls')), #URL /question/<ID>
+    url(r'^ask/', include('qa.urls')), #URL /ask/
+    url(r'^popular/', include('qa.urls')), #URL /popular/
+    url(r'^new/', include('qa.urls')), #URL /new/   
 ]
+
